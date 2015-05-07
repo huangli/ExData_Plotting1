4c06356cfea8e9164b0a42408c4d6bccd08bdbfb
huangli <ekinasm@gmail.com>
2015-05-07 16:52:43 +0800

    plot1
    fix bugs

---
+++ plot1.R
@@ -0,0 +1,14 @@
+
+#1. download the file and read into table
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile="./myfile.zip")
+unzip("myfile.zip")
+mydf <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors=FALSE)
+
+#2. filter the date from 2007-02-01 and 2007-02-02
+library(dplyr)
+mydf <- mutate(mydf, Date_format = as.Date(Date, "%d/%m/%Y"))
+mydf <- filter(mydf, Date_format >= as.Date("2007-02-01"), Date_format <= as.Date("2007-02-02"))
+
+#3. plot 
+hist(as.numeric(mydf$Global_active_power))
